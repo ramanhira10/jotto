8b500ded99d70d224280bf1df3f44513a9462f7d
ramanhira10 <ramanhira@gmail.com>
2021-04-29 01:22:09 +0530

    WIP: Added Congrats component and testUtils

---
+++ src/Congrats.js
@@ -0,0 +1,20 @@
+//recieve the success state as a prop
+import React from 'react';
+/**
+ * Functional react component for congratulatory message.
+ * @param {object} props - React props
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats'></div>;
+  }
+};
