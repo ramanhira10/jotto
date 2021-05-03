ad1203f20701cce90ba55b89a6fb92342f3ce6e6
ramanhira10 <ramanhira@gmail.com>
2021-05-03 21:16:31 +0530

    WIP: added prop types to the component

--- src/Input.js
+++ src/Input.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import propTypes from 'prop-types';
 
-const Input = props => {
+const Input = ({ secretWord = '' }) => {
   return <div data-test='component-input' />;
+};
+
+Input.propTypes = {
+  secretWord: propTypes.string.isRequired
 };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -3,15 +3,16 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
-const defaultProps = {};
-
-const setup = (props = {}) => {
-  const setupProps = { ...defaultProps, props };
-  return shallow(<Input {...setupProps} />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test('renders without errors', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('should not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: '' });
 });
